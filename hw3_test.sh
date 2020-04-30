55794a7288e9a2e510a7e2241e43153b07d50b4d
Leng-Kai <45026969+Leng-Kai@users.noreply.github.com>
2020-04-30 20:17:22 +0800

    Update hw3_test.sh

--- hw3_test.sh
+++ hw3_test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget https://github.com/NTU-speech-lab/hw3-Leng-Kai/releases/download/0.0.0/my_model_best.pth
+wget --no-check-certificate https://github.com/NTU-speech-lab/hw3-Leng-Kai/releases/download/0.0.0/my_model_best.pth
 python3 hw3_test.py $1 $2
